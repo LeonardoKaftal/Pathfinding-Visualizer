8532316b0f25a512bf0f5f0d44221df76cd4f365
Leonardo Kaftal <leonardokaftal1@gmail.com>
2022-06-28 12:20:42 +0200

    Added the css file

--- dist/bundle3fa53da1c956a4a6ba5d.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-alert("a")
-/******/ })()
-;
-//# sourceMappingURL=bundle3fa53da1c956a4a6ba5d.js.map
