99453a2f4d1566776a853e9544467cd11e610d70
Leonardo Kaftal <leonardokaftal1@gmail.com>
2022-06-28 12:07:27 +0200

    basic structure of the project

---
+++ dist/bundle3fa53da1c956a4a6ba5d.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert("a")
+/******/ })()
+;
+//# sourceMappingURL=bundle3fa53da1c956a4a6ba5d.js.map
